635846059b04d9665cca3308a01729641c0e4c45
kevinRodriguezCastro <10208@cifpceuta.es>
2024-01-09 18:06:11 +0100

    a

---
+++ src/main/java/org/example/ConversorObjeto.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+public class ConversorObjeto {
+
+    // Sax
+    // gson
+    private Persona persona;
+    public void jsonObjeto(String json){
+        Gson g = new Gson();
+        persona = g.fromJson(json,Persona.class);
+    }
+    public void xmlObjeto(String xml){
+
+    }
+    public void yamlObjeto(String yaml){
+
+    }
+
+    public String xml(){
+        return "<Persona>\n\t<Nombre> "+persona.getNombre()+" </Nombre>\n\t<Apellidos> "+persona.getApellidos()+" </Apellidos>\n\t<Edad> "+persona.getEdad()+" </Edad>\n</Persona>";
+    }
+    public String json() {
+        return "{\n\t'Nombre':'"+persona.getNombre()+"',\n\t'Apellidos':'"+persona.getApellidos()+"',\n\t'Edad':'"+persona.getEdad()+"'\n}";
+    }
+    public String yaml(){
+        return "Persona:\n\tNombre:"+persona.getNombre()+"\n\tApellidos:"+persona.getApellidos()+"\n\tEdad:"+persona.getEdad();
+    }
+}
